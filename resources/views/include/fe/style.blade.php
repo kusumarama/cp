429c378a6f6542d828f69d479eed4f4df5a867c2
kusumarama <rama.kuusuma@gmail.com>
2025-11-26 09:47:00 +0700

    Add cache busting parameter to styles.css

--- resources/views/include/fe/style.blade.php
+++ resources/views/include/fe/style.blade.php
@@ -4,5 +4,5 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{ asset ('template_fe/css/styles.css')}}" rel="stylesheet" />
+        <link href="{{ asset ('template_fe/css/styles.css') }}?v={{ time() }}" rel="stylesheet" />
         <link href="{{ asset ('template_fe/css/loader.css')}}" rel="stylesheet" />
