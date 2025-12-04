66a549d5ecbc7523f47cb2e3658c113ae46b016a
kusumarama <rama.kuusuma@gmail.com>
2025-12-04 13:31:59 +0700

    Add complete multilingual support with EN/ID translations

---
+++ resources/lang/en/common.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+return [
+    // Navigation
+    'home' => 'Home',
+    'our_professionals' => 'Our Professionals',
+    'board_of_directors' => 'Board of Directors',
+    'management' => 'Management',
+    'portfolio' => 'Portfolio',
+    'design' => 'Design',
+    'legality' => 'Legality',
+    
+    // Board of Directors
+    'board_title' => 'Board of Directors',
+    'board_subtitle' => 'Leading our company with vision and integrity',
+    'management_title' => 'Management',
+    'management_subtitle' => 'Our dedicated team driving operational excellence',
+];
---
+++ resources/lang/id/common.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+return [
+    // Navigation
+    'home' => 'Beranda',
+    'our_professionals' => 'Tim Profesional Kami',
+    'board_of_directors' => 'Dewan Direksi',
+    'management' => 'Manajemen',
+    'portfolio' => 'Portofolio',
+    'design' => 'Desain',
+    'legality' => 'Legalitas',
+    
+    // Board of Directors
+    'board_title' => 'Dewan Direksi',
+    'board_subtitle' => 'Memimpin perusahaan kami dengan visi dan integritas',
+    'management_title' => 'Manajemen',
+    'management_subtitle' => 'Tim dedikasi kami yang mendorong keunggulan operasional',
+];
