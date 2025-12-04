3729c3781f9ba5a4a5899894acc72a9802c55803
kusumarama <rama.kuusuma@gmail.com>
2025-12-04 11:31:56 +0700

    Remove sb-forms-latest.js causing GET_ELEMENTS error on pages without forms

--- resources/views/include/fe/script.blade.php
+++ resources/views/include/fe/script.blade.php
@@ -3,4 +3,4 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="{{asset ('template_fe/js/scripts.js')}}"></script>
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+        
